2926bb75f458494dbae71ccc05903a05c84b6ba7
Samy Dindane <samy@dindane.com>
2019-03-08 02:43:20 +0100

    Add a simple version that works with pinned tabs + single window

--- simplefocus.js
+++ simplefocus.js
@@ -1,5 +1,35 @@
-function focus() {
-  console.log('Hello');
+browser.browserAction.onClicked.addListener(focus);
+
+var windows;
+var tabs;
+
+async function focus(currentTab) {
+  windows = await browser.windows.getAll();
+  tabs = await browser.tabs.query({});
+  var tabsWithoutTheCurrentOne = tabs.filter(item => item.id != currentTab.id);
+
+  console.log('Saved', windows.length, 'window(s) and', tabs.length, 'tab(s).')
+
+  tabsWithoutTheCurrentOne.map(tab => {
+    if (tab.pinned)
+      return;
+    return browser.tabs.remove(tab.id);
+  });
+
+  browser.tabs.onRemoved.addListener(async (tabId) => {
+    if (currentTab.id != tabId)
+      return;
+
+    await openClosedTabs(tabsWithoutTheCurrentOne);
+  });
 }
 
-browser.browserAction.onClicked.addListener(focus);
+async function openClosedTabs(tabs) {
+  tabs.map(async tab => {
+    if (tab.url.startsWith('about:'))
+      return;
+
+    console.log('Reopening', tab.url);
+    await browser.tabs.create({ url : tab.url });
+  });
+}
