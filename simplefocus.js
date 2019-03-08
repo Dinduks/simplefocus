38291cf1565434d3ff5a4519f10ea02d42c31133
Samy Dindane <samy@dindane.com>
2019-03-08 03:29:51 +0100

    Support the case where there's no pinned tab opened

--- simplefocus.js
+++ simplefocus.js
@@ -1,26 +1,57 @@
 browser.browserAction.onClicked.addListener(focus);
 
-var windows;
-var tabs;
+window.simplefocus = {
+  windows: undefined,
+  tabs: undefined,
+  temporaryPinnedTab: undefined,
+};
 
-async function focus(currentTab) {
-  windows = await browser.windows.getAll();
-  tabs = await browser.tabs.query({});
-  var tabsWithoutTheCurrentOne = tabs.filter(item => item.id != currentTab.id);
-
-  console.log('Saved', windows.length, 'window(s) and', tabs.length, 'tab(s).')
-
-  tabsWithoutTheCurrentOne.map(tab => {
+async function closeTabs(tabs) {
+  tabs.map(tab => {
     if (tab.pinned)
       return;
+
     return browser.tabs.remove(tab.id);
   });
+}
+
+async function createTemporaryPinnedTab(tabs) {
+  if (tabs.filter(tab => tab.pinned).length > 0)
+    return;
+
+  console.log('Creating the temporary pinned tab.')
+  window.simplefocus.temporaryPinnedTab = await browser.tabs.create({
+    url : 'https://samy.dindane.com',
+    pinned: true,
+    active: false
+  });
+}
+
+async function removeTemporaryPinnedTab() {
+  console.log('Removing the temporary pinned tab.')
+  browser.tabs.remove(window.simplefocus.temporaryPinnedTab.id);
+}
+
+async function focus(currentTab) {
+  if (currentTab.pinned)
+    return;
+
+  window.simplefocus.windows = await browser.windows.getAll();
+  window.simplefocus.tabs = await browser.tabs.query({});
+
+  var tabsExceptTheCurrentOne = window.simplefocus.tabs.filter(item => item.id != currentTab.id);
+
+  console.log('Saved', window.simplefocus.windows.length, 'window(s) and', window.simplefocus.tabs.length, 'tab(s).')
+
+  closeTabs(tabsExceptTheCurrentOne);
+
+  createTemporaryPinnedTab(window.simplefocus.tabs);
 
   browser.tabs.onRemoved.addListener(async (tabId) => {
     if (currentTab.id != tabId)
       return;
 
-    await openClosedTabs(tabsWithoutTheCurrentOne);
+    await openClosedTabs(tabsExceptTheCurrentOne);
   });
 }
 
@@ -32,4 +63,6 @@ async function openClosedTabs(tabs) {
     console.log('Reopening', tab.url);
     await browser.tabs.create({ url : tab.url });
   });
+
+  removeTemporaryPinnedTab();
 }
